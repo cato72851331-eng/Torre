0f2f3b75f42cffecc713011911fff06af39e540b
Iver Alberto Cruz colque <cato72851331@gmail.com>
2026-09-10 01:39:59 -0400

    Mostrar Tablero

--- Torre/Torre.cpp
+++ Torre/Torre.cpp
@@ -43,6 +43,47 @@ public:
         return false;
     }
 
+    void MostrarTablero()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (i == fila && j == columna)
+                    cout << "T ";
+                else
+                    cout << ". ";
+            }
+
+            cout << endl;
+        }
+    }
+
     
 };
+
+int main()
+{
+    Torre torre;
+
+    cout << "POSICION INICIAL:" << endl;
+    torre.Visualizar();
+
+    cout << "\nTABLERO:" << endl;
+    torre.MostrarTablero();
+
+    cout << "\nMOVIENDO TORRE A (0,5):" << endl;
+
+    if (torre.Mover(0, 5))
+        cout << "Movimiento valido." << endl;
+    else
+        cout << "Movimiento invalido." << endl;
+
+    torre.Visualizar();
+
+    cout << "\nTABLERO:" << endl;
+    torre.MostrarTablero();
+
+    return 0;
+}
 
