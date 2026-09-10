fc1922cb9b5eb545ab2fb861040cf4906e79156a
Iver Alberto Cruz colque <cato72851331@gmail.com>
2026-09-10 01:14:37 -0400

    Movimiento Torre

--- Torre/Torre.cpp
+++ Torre/Torre.cpp
@@ -23,11 +23,40 @@ public:
         cout << "Fila: " << fila << endl;
         cout << "Columna: " << columna << endl;
     }
+
+    bool Mover(short nuevaFila, short nuevaColumna)
+    {
+        if (nuevaFila < 0 || nuevaFila > 7 ||
+            nuevaColumna < 0 || nuevaColumna > 7)
+        {
+            return false;
+        }
+
+        if (nuevaFila == fila || nuevaColumna == columna)
+        {
+            fila = nuevaFila;
+            columna = nuevaColumna;
+            return true;
+        }
+
+        return false;
+    }
 };
 
 int main()
 {
     Torre torre;
+
+    torre.Visualizar();
+
+    if (torre.Mover(0, 5))
+    {
+        cout << "Movimiento valido." << endl;
+    }
+    else
+    {
+        cout << "Movimiento invalido." << endl;
+    }
 
     torre.Visualizar();
 
